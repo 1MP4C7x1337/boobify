c60f854a369eaadbea57a7f14b8b95293fb1f33a
Mateusz Dutkiewicz <matias.devacc@gmail.com>
2021-08-11 14:14:05 +0200

    .

--- resources/views/testpayments.blade.php
+++ resources/views/testpayments.blade.php
@@ -4,7 +4,7 @@
 
 <div>
     <a class="buy-with-crypto"
-       href="https://commerce.coinbase.com/checkout/d81f0efd-96dc-4cce-b30a-b90fa98ccd78">
+       href="https://commerce.coinbase.com/checkout/d62f8dc6-1294-4e20-a894-fc0bb8ea9f1c">
       Buy with Crypto
     </a>
     <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807">
